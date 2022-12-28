eb175d717a5b8cb16660689d651b492b9efd971a
sorkofi <kinanprincipes@gmail.com>
2022-12-28 19:41:26 -0400

    added top-bar menu buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import reactLogo from './assets/react.svg'
 
 import './styles/global.css'
 
+const menus = [
+  "Home",
+  "About",
+  "Skills",
+  "Projects"
+]
+
 function App() {
   
   return (
     <div className="container">
 
       <div className="top-bar">
-        <p>top bar</p>
+        {
+          menus.map((menus) => <button className="button-global menu">{menus}</button>)
+        }
       </div>
+      
       
       <div className="header">
         <p>header</p>
+        <button className='button-global'>header</button>
       </div>
-      
+
     </div>
   )
 }
